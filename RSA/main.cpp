d5a273d0ff8a39b7c52127c8294cf7863442a8cb
TheJonBovi <deraitaa@gmail.com>
2018-02-25 10:44:42 -0800

    Creation of project
    Created cpp and h files for each aspect of project.
    Menu re-used from previous projects

---
+++ RSA/main.cpp
@@ -0,0 +1,63 @@
+// main.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "keygen.h"
+#include "encryption.h"
+#include "signature.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int choice;
+	bool programRun = true;
+
+	while (programRun)
+	{
+		cout << "************RSA Multitool************\n";
+		cout << endl;
+		cout << " 1 - Key Generation\n";
+		cout << " 2 - Encrypt\n";
+		cout << " 3 - Decrypt\n";
+		cout << " 4 - Exit\n";
+		cout << endl;
+		cout << " Enter your choice and press return: ";
+
+		cin >> choice;
+
+		cout << endl;
+
+		switch (choice)
+		{
+		case 1:
+			{
+
+			break;
+			}
+
+		case 2:
+			{
+
+			break;
+			}
+
+		case 3: 
+			cout << " End of Program.\n";
+			programRun = false;
+			return 0;
+
+			break;
+
+		default: 
+			cout << " Not a Valid Choice. Choose again. \n";
+			cin >> choice;
+			cout << endl;
+
+			break;
+		}
+	}
+
+    return 0;
+}
